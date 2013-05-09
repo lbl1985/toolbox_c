75e1e3fc8920c7f055ad5fb341b063e5fed97549
Binlong Li <herbert19lee@gmail.com>
2013-05-09 08:56:36 -0400

    add stdafx header, (this could be a trouble maker later) switch from individual test into batch tests add test for num2str2

--- test_util.cpp
+++ test_util.cpp
@@ -6,7 +6,12 @@
 //  Copyright (c) 2013 Binlong Li. All rights reserved.
 //
 
+#ifdef _WIN32 || _WIN64
+
+#include "stdafx.h"
+
 #include "util.h"
+
 
 // -----------------------------------------------------
 // --------------- TEST: VideoUtil Class ---------------
@@ -55,7 +60,6 @@ int ImageResize(){
     
     for (int i = 1; i <= 3; i++) {
         cv::namedWindow(str[i-1], CV_WINDOW_AUTOSIZE);
-        ImageUtil::resize(dst, src, 1/(pow(2, i)));
         ImageUtil::resize(dst, src, 1/(cv::pow(static_cast<double>(2), static_cast<double>(i))));
         cv::imshow(str[i-1], dst);
     }
@@ -88,7 +92,7 @@ bool minValueIndexTest(){
     std::vector<int>::iterator result = std::min_element(dVec.begin(), dVec.end());
     long index = std::distance(dVec.begin(), result);
     
-    std::cout << "The min element is at: " << *result << " at: " << index << std::endl;
+    //std::cout << "The min element is at: " << *result << " at: " << index << std::endl;
     if (index != 7) {
         return false;
     }
@@ -107,10 +111,10 @@ bool sort_indexTest(){
     
     std::vector<size_t> res_index;
     res_index = NumericUtil::sort_index(dVec, sorted);
-    std::for_each(res_index.begin(), res_index.end(), std::cout << _1 << " ");
-    std::cout << std::endl;
-    std::for_each(sorted.begin(), sorted.end(), std::cout << _1 << " ");
-    std::cout << std::endl;
+    //std::for_each(res_index.begin(), res_index.end(), std::cout << _1 << " ");
+    //std::cout << std::endl;
+    //std::for_each(sorted.begin(), sorted.end(), std::cout << _1 << " ");
+    //std::cout << std::endl;
     return true;
 }
 
@@ -121,10 +125,11 @@ bool sort_indexTest(){
 // -----------------------------------------------------
 bool fullfileTest(){
     std::string path = fullfile(3, "/Users/herbert19lee/Develop", "HRF", "Util");
+
     if (path.compare("/Users/herbert19lee/Develop/HRF/Util") != 0) {
         return false;
     }
-    std::cout << path << std::endl;
+    //std::cout << path << std::endl;
     return true;
 }
 
@@ -145,27 +150,76 @@ bool fullfileTest(){
 //}
 //checkFolder(fullfile(3, "/Users/herbert19lee", "Desktop",  "test"));
 
+// -----------------------------------------------------
+// ---------------  TEST3: num2str2  -------------
+// -----------------------------------------------------
+bool num2str2Test(){
+	int intNum = 20;
+	long longNum = 200000;
+	double doubleNum = 20.3;
+	double negNum = -20.3;
+	int len = 5;
+
+	std::string intStr(num2str2<int>(intNum, len));
+	std::string longStr(num2str2<long>(longNum, len));
+	std::string doubleStr(num2str2<double>(doubleNum, len));
+	std::string negStr(num2str2<double>(negNum, len));
+
+	if (intStr.compare("00020") != 0 || longStr.compare("200000") != 0 || doubleStr.compare("020.3") != 0 || negStr.compare("-20.3") != 0 )
+	{
+		return false;
+	}
+	return true;
+}
+
 // main controller function
 int main(){
-    std::string testName("fullfileTest");
+
+	/*std::string testName("fullfileTest");
+
+	if (testName.compare("VideoShow") == 0) {
+	VideoShowTest();
+	}else if (testName.compare("VideoSave") == 0){
+	VideoSaveTest();
+	}else if (testName.compare("ImageResize") == 0){
+	ImageResize();
+	}else if (testName.compare("minValueIndex") == 0){
+	minValueIndexTest();
+	}else if (testName.compare("combineTwoImages") == 0){
+	combineTwoImagesTest();
+	}else if (testName.compare("sort_indexTest") == 0){
+	sort_indexTest();
+	}else if (testName.compare("fullfileTest") == 0){
+	fullfileTest();
+	}*/
+	bool allPass = true;
+	if (!minValueIndexTest())
+	{
+		std::cerr << "minValueIndexTest Failed;" << std::endl;
+		allPass = false;
+	}
+
+	if(!sort_indexTest()){
+		std::cerr << "sort_indexTest Failed;" << std::endl;
+		allPass = false;
+	}
+
+	if (!fullfileTest())
+	{
+		std::cerr << "fullfileTest Failed;" << std::endl;
+		allPass = false;
+	}
     
-    if (testName.compare("VideoShow") == 0) {
-        VideoShowTest();
-    }else if (testName.compare("VideoSave") == 0){
-        VideoSaveTest();
-    }else if (testName.compare("ImageResize") == 0){
-        ImageResize();
-    }else if (testName.compare("minValueIndex") == 0){
-        minValueIndexTest();
-    }else if (testName.compare("combineTwoImages") == 0){
-        combineTwoImagesTest();
-    }else if (testName.compare("sort_indexTest") == 0){
-        sort_indexTest();
-    }else if (testName.compare("fullfileTest") == 0){
-        fullfileTest();
-    }
-    
-    
+    if(!num2str2Test()){
+		std::cerr << "num2str2Test Failed" << std::endl;
+		allPass = false;
+	}
+
+	if (allPass)
+	{
+		std::cout << "ALL util tests PASS!!!" << std::endl;
+	}
+
     return 0;
 }
 
